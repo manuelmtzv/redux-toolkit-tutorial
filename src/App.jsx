200fb5e9d002d4b70fb189021f08ce1b4be7e48c
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-05-16 20:43:57 -0600

    Creando estructura de react y vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+        commodi repudiandae qui provident numquam fugit sed! Cupiditate saepe
+        quos modi asperiores repellat magnam nostrum quo, error accusantium eos
+        earum veniam.
+      </p>
+    </div>
+  )
+}
